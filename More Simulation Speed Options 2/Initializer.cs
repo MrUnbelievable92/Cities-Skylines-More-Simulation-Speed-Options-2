0b8eab3e6a458a20373ed7e02b7eee21bd55d1dc
Maximilian Kalimon <mkalimon@gmx.de>
2024-10-05 01:50:15 +0200

    Add files via upload

---
+++ More Simulation Speed Options 2/Initializer.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using ColossalFramework.UI;
+
+namespace MoreSimulationSpeedOptions2
+{
+    public class Initializer : MonoBehaviour
+    {
+        private void Start()
+        {
+            UIMultiStateButton speedBar = UIView.GetAView().FindUIComponent<UIMultiStateButton>("Speed");
+            GameObject go = new GameObject();
+            go.transform.parent = speedBar.transform.parent.transform;
+            UIButton speedButton = go.AddComponent<UIButton>();
+            
+            speedButton.width = speedBar.width;
+            speedButton.height = speedBar.height;
+            speedButton.transformPosition = speedBar.transformPosition;
+            speedButton.transform.position = speedBar.transform.position;
+
+            speedBar.enabled = false;
+
+            Color32 textColor = new Color32(0, 255, 0, 255);
+                
+            speedButton.text = "x1";
+            speedButton.textColor = textColor;
+            speedButton.focusedTextColor = textColor;
+            speedButton.hoveredTextColor = textColor;
+            speedButton.pressedTextColor = textColor;
+
+            speedButton.normalBgSprite = "ButtonMenu";
+            speedButton.disabledBgSprite = "ButtonMenuDisabled";
+            speedButton.hoveredBgSprite = "ButtonMenuHovered";
+            speedButton.focusedBgSprite = "ButtonMenu";
+            speedButton.pressedBgSprite = "ButtonMenuPressed";
+
+            speedButton.playAudioEvents = true;
+
+            speedButton.eventMouseUp += Mod.OnMouseUp;
+
+            Mod.speedButton = speedButton;
+
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ More Simulation Speed Options 2/Mod.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using ColossalFramework;
+using ColossalFramework.UI;
+using ICities;
+
+namespace MoreSimulationSpeedOptions2
+{
+    public class Mod : LoadingExtensionBase, IUserMod
+    {
+        internal static UIButton speedButton;
+
+        public string Name => "More Simulation Speed Options 2";
+        public string Description => "Adds more simulation speed options.";
+
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+        	new GameObject().AddComponent<Initializer>();
+        }
+         
+        public override void OnLevelUnloading()
+        {
+        	Object.Destroy(speedButton.gameObject);
+        }
+
+        unsafe internal static void OnMouseUp(UIComponent c, UIMouseEventParameter p)
+        {
+            int speed = Singleton<SimulationManager>.instance.SelectedSimulationSpeed;
+            Color32 textColor = new Color32(0, 0, 0, 255);
+
+            if ((p.buttons & UIMouseButton.Left) != 0)
+            {
+                switch (speed)
+                {
+                    case 1: speed = 2; textColor.r = 87;  textColor.g = 165; break;
+                    case 2: speed = 4; textColor.r = 245; textColor.g = 216; break;
+                    case 3:
+                    case 4: speed = 6; textColor.r = 255; textColor.g = 130; break;
+                    case 6: speed = 9; textColor.r = 255; textColor.g = 0;   break;
+                    case 9: speed = 1; textColor.r = 0;   textColor.g = 255; break;
+                }
+            }
+            else if ((p.buttons & UIMouseButton.Right) != 0)
+            {
+                switch (speed)
+                {
+                    case 1: speed = 9; textColor.r = 255; textColor.g = 0;   break;
+                    case 2: speed = 1; textColor.r = 0;   textColor.g = 255; break;
+                    case 3:
+                    case 4: speed = 2; textColor.r = 87;  textColor.g = 165; break;
+                    case 6: speed = 4; textColor.r = 245; textColor.g = 216; break;
+                    case 9: speed = 6; textColor.r = 255; textColor.g = 130; break;
+                }
+            }
+            else
+            {
+                return;
+            }
+
+            Singleton<SimulationManager>.instance.SelectedSimulationSpeed = speed;
+
+            char* text = stackalloc char[]
+            {
+                'X',
+                (char)('0' + speed)
+            };
+            speedButton.text = new string(text, 0, 2);
+
+            speedButton.textColor = textColor;
+            speedButton.focusedTextColor = textColor;
+            speedButton.hoveredTextColor = textColor;
+            speedButton.pressedTextColor = textColor;
+        }
+    }
+}
